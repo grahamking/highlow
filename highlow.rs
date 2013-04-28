07be9b8874cff4219d46e98a9207c576e544adcd
Graham King <graham@gkgk.org>
2013-04-28 13:13:24 -0700

    Added symbol to prices table

--- highlow.rs
+++ highlow.rs
@@ -8,50 +8,23 @@ use core::clone;
 use core::to_str;
 
 struct Price {
+    symbol: ~str,
     date: ~str,
-    close: ~str
+    price: ~str
 }
 
 impl clone::Clone for Price {
     fn clone(&self) -> Price {
-        Price{date: self.date.clone(), close: self.close.clone()}
+        Price{
+            symbol: self.symbol.clone(),
+            date: self.date.clone(),
+            price: self.price.clone()}
     }
 }
 impl to_str::ToStr for Price {
     fn to_str(&self) -> ~str {
-        fmt!("%s: %s", self.date, self.close)
+        fmt!("%s %s: %s", self.symbol, self.date, self.price)
     }
-}
-
-fn load(filename: &str) -> ~[~str] {
-
-    let fres = io::file_reader(~path::Path(filename));
-    let f = result::unwrap(fres);
-    return f.read_lines();
-}
-
-fn parse(lines: &[~str]) -> ~[Price] {
-
-    let mut date = ~"";
-    let mut close = ~"";
-    let mut i = 0;
-    let mut prices: ~[Price] = ~[];
-
-    for lines.each |&line| {
-
-        i = 0;
-        for line.each_split_char(',') |piece| {
-            if i == 0 {
-                date = piece.to_str();
-            } else if i == 4 {
-                close = piece.to_str();
-            }
-            i += 1;
-        }
-        prices += [Price{date: date.clone(), close: close.clone()}]
-    }
-
-    return prices;
 }
 
 struct DB {
@@ -75,18 +48,19 @@ impl DB {
     }
 
     fn create(&self) -> Result<sqlite::ResultCode, sqlite::ResultCode>  {
-        self.database.exec("CREATE TABLE prices (date text, price real)")
+        self.database.exec(
+            "CREATE TABLE prices (symbol text, date text, price real)")
     }
 
-    fn write_price(&self, date: ~str, price: float) {
+    fn write_price(&self, symbol: &str, date: &str, price: float) {
         let res = self.database.exec(fmt!(
-            "INSERT INTO prices VALUES ('%s', %f)", date, price));
+            "INSERT INTO prices VALUES ('%s', '%s', %f)", symbol, date, price));
         if ! res.is_ok() {
             println(fmt!("INSERT error: %s", res.unwrap_err().to_str()));
         }
     }
 
-    fn _min_max(&self, min_max: &str) -> ~Price {
+    fn _min_max(&self, min_max: &str, symbol: &str) -> ~Price {
 
         let extra =
             match min_max {
@@ -94,23 +68,60 @@ impl DB {
                 _ => ""
             };
         let sql = fmt!(
-            "SELECT date, price FROM prices ORDER BY price %s LIMIT 1",
+            "SELECT symbol, date, price FROM prices WHERE symbol = '%s' ORDER BY price %s LIMIT 1",
+            symbol,
             extra);
 
         let st: sqlite::Cursor = self.database.prepare(sql, &None).unwrap();
         st.step();
-        let date = st.get_text(0);
-        let price = st.get_num(1);
-        return ~Price{date:date, close:fmt!("%f", price)}
+        let symbol = st.get_text(0);
+        let date = st.get_text(1);
+        let price = st.get_num(2);
+        return ~Price{symbol: symbol, date:date, price:fmt!("%f", price)}
     }
 
-    fn max_price(&self) -> ~Price {
-        self._min_max("MAX")
+    fn max_price(&self, symbol: &str) -> ~Price {
+        self._min_max("MAX", symbol)
     }
 
-    fn min_price(&self) -> ~Price {
-        self._min_max("MIN")
+    fn min_price(&self, symbol: &str) -> ~Price {
+        self._min_max("MIN", symbol)
     }
+}
+
+fn load(filename: &str) -> ~[~str] {
+
+    let fres = io::file_reader(~path::Path(filename));
+    let f = result::unwrap(fres);
+    return f.read_lines();
+}
+
+fn parse(symbol: &str, lines: &[~str]) -> ~[Price] {
+
+    let mut date = ~"";
+    let mut close = ~"";
+    let mut i = 0;
+    let mut prices: ~[Price] = ~[];
+
+    for lines.each |&line| {
+
+        i = 0;
+        for line.each_split_char(',') |piece| {
+            if i == 0 {
+                date = piece.to_str();
+            } else if i == 4 {
+                close = piece.to_str();
+            }
+            i += 1;
+        }
+        prices += [Price{
+            symbol: symbol.to_str(),
+            date: date.clone(),
+            price: close.clone()}
+        ]
+    }
+
+    return prices;
 }
 
 fn save(prices: ~[Price]) {
@@ -136,26 +147,26 @@ fn save(prices: ~[Price]) {
     for prices.each |&price| {
         println(price.to_str());
 
-        val = float::from_str(price.close).unwrap();
-        db.write_price(price.date.to_str(), val);
+        val = float::from_str(price.price).unwrap();
+        db.write_price(price.symbol.to_str(), price.date.to_str(), val);
     }
 
 }
 
-fn cmd_init() {
-    let all_lines: ~[~str] = load("vt.csv");
+fn cmd_load(symbol: &str, filename: &str) {
+    let all_lines: ~[~str] = load(filename);
 
     // 'tail' because the first line is the column headers, which we
     // don't want.
     let lines: &[~str] = all_lines.tail();
 
-    let prices: ~[Price] = parse(lines);
+    let prices: ~[Price] = parse(symbol, lines);
     println(fmt!("%?", prices));
 
     save(prices);
 }
 
-fn cmd_max() {
+fn cmd_max(symbol: &str) {
 
     let res = DB::open("test.db");
     if ! res.is_ok() {
@@ -163,35 +174,36 @@ fn cmd_max() {
     }
     let db = res.unwrap();
 
-    println(db.max_price().to_str());
+    println(db.max_price(symbol).to_str());
 }
 
-fn cmd_min() {
+fn cmd_min(symbol: &str) {
     let res = DB::open("test.db");
     if ! res.is_ok() {
         return;
     }
     let db = res.unwrap();
 
-    println(db.min_price().to_str());
+    println(db.min_price(symbol).to_str());
 }
 
 fn main() {
 
-    static USAGE: &'static str = "Usage: highlow init|max|min";
+    static USAGE: &'static str = "Usage: highlow load <symbol> <filename>|max <symbol>|min <symbol>";
 
     let args: &[~str] = os::args();
-    if args.len() != 2 {
+    if args.len() < 3 {
         println(USAGE);
         return;
     }
 
     let cmd: ~str = args[1].clone();
+    let symbol: ~str = args[2].clone();
 
     match cmd {
-        ~"init" => cmd_init(),
-        ~"max" => cmd_max(),
-        ~"min" => cmd_min(),
+        ~"load" => cmd_load(symbol, args[3]),
+        ~"max" => cmd_max(symbol),
+        ~"min" => cmd_min(symbol),
         other => println(fmt!("Invalid cmd: %s", other))
     }
 }
